7a034cb513ecd92bc17553308348843ead145fc1
Franco Stazzone <fralstazzone@gmail.com>
2025-04-15 16:25:01 -0300

    Adds a children comments attribute

--- src/models/comment.py
+++ src/models/comment.py
@@ -4,14 +4,16 @@ class Comment:
     def __init__(self,
                  author: str,
                  body: str,
+                 edited_timestamp: int,
                  upvotes: int,
                  downvotes: int,
                  created_utc: int,
-                 edited_timestamp: int = None
+                 children: list["Comment"]
                  ):
         self.author = author
         self.body = body
+        self.edited_timestamp = edited_timestamp
         self.upvotes = upvotes
         self.downvotes = downvotes
         self.created_utc = created_utc
-        self.edited_timestamp = edited_timestamp
+        self.children = children
