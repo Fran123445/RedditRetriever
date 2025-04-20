258be0ae3690b2d2a05909c9f46730d1b074d949
Franco Stazzone <fralstazzone@gmail.com>
2025-04-19 21:33:02 -0300

    Adds a creation script

---
+++ db/creation_script.sql
@@ -0,0 +1,72 @@
+-- DB Creation
+
+CREATE DATABASE RedditRetriever
+GO
+
+USE RedditRetriever
+GO
+
+CREATE TABLE Subreddit(
+	id INTEGER PRIMARY KEY,
+	internal_reddit_id NVARCHAR(16) UNIQUE NOT NULL,
+	name NVARCHAR(512) NOT NULL,
+	subscribers INTEGER NOT NULL,
+	nsfw BIT NOT NULL
+)
+
+CREATE TABLE Flair(
+	id BIGINT PRIMARY KEY,
+	text NVARCHAR(512)
+)
+
+CREATE TABLE [User](
+	id BIGINT PRIMARY KEY,
+	internal_reddit_id NVARCHAR(16) UNIQUE NOT NULL,
+	username NVARCHAR(512) NOT NULL
+)
+
+CREATE TABLE Author(
+	id BIGINT,
+	subreddit_id INTEGER NOT NULL,
+	flair_id BIGINT,
+	PRIMARY KEY (id, subreddit_id),
+	FOREIGN KEY (id) REFERENCES [User](id),
+	FOREIGN KEY (subreddit_id) REFERENCES Subreddit(id),
+	FOREIGN KEY (flair_id) REFERENCES Flair(id)
+)
+
+CREATE TABLE Post(
+	id BIGINT PRIMARY KEY,
+	internal_reddit_id NVARCHAR(16) UNIQUE NOT NULL,
+	subreddit_id INTEGER NOT NULL,
+	author_id BIGINT NOT NULL,
+	flair_id BIGINT,
+	title NVARCHAR(MAX) NOT NULL,
+	body NVARCHAR(MAX),
+	edited_date DATETIME, -- if this one's null, then the post was never edited
+	upvotes INTEGER NOT NULL,
+	downvotes INTEGER NOT NULL,
+	nsfw BIT NOT NULL,
+	spoiler BIT NOT NULL,
+	creation_date DATETIME NOT NULL,
+	FOREIGN KEY (subreddit_id) REFERENCES Subreddit(id),
+	FOREIGN KEY (author_id, subreddit_id) REFERENCES Author(id, subreddit_id),
+	FOREIGN KEY (flair_id) REFERENCES Flair(id)
+)
+
+CREATE TABLE Comment(
+	id BIGINT PRIMARY KEY,
+	internal_reddit_id NVARCHAR(16) UNIQUE NOT NULL,
+	post_id BIGINT NOT NULL,
+	author_id BIGINT NOT NULL,
+	subreddit_id INTEGER NOT NULL,
+	parent_comment_id BIGINT, -- if this one's null then it's a comment to the op
+	body NVARCHAR(MAX) NOT NULL,
+	edited_date DATETIME,
+	upvotes INTEGER NOT NULL,
+	downvotes INTEGER NOT NULL,
+	creation_date DATETIME NOT NULL,
+	FOREIGN KEY (post_id) REFERENCES Post(id),
+	FOREIGN KEY (author_id, subreddit_id) REFERENCES Author(id, subreddit_id),
+	FOREIGN KEY (parent_comment_id) REFERENCES Comment(id)
+)
