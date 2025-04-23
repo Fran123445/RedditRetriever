b7c8330d8208a5d73058c8453b51702225fde7a9
Franco Stazzone <fralstazzone@gmail.com>
2025-04-22 21:21:03 -0300

    Adds a comment staging transformer

---
+++ src/transformers/staging/comment_staging_transformer.py
@@ -0,0 +1,41 @@
+from src.models.comment import Comment
+from src.models.post import Post
+from src.models.subreddit import Subreddit
+from src.transformers.staging.staging_transformer import StagingTransformer
+
+
+class CommentStagingTransformer(StagingTransformer):
+
+    def transform(self,
+                  subreddit: Subreddit):
+
+        staging_comments_list = []
+
+        for post in subreddit.posts:
+            self._transform_comments(subreddit, post, None, post.comments, staging_comments_list)
+
+        return staging_comments_list
+
+    def _transform_comments(self,
+                            subreddit: Subreddit,
+                            post: Post,
+                            parent_comment: Comment,
+                            comments: list[Comment],
+                            staging_comments_list: list):
+
+        for comment in comments:
+            staging_comments_list.append(
+                (comment.comment_id,
+                 post.post_id,
+                 comment.author.author_id,
+                 subreddit.subreddit_id,
+                 parent_comment.comment_id if parent_comment else None,
+                 comment.body,
+                 comment.edited_timestamp,
+                 comment.upvotes,
+                 comment.downvotes,
+                 comment.created_utc)
+            )
+
+            if comment.children:
+                self._transform_comments(subreddit, post, comment, comment.children, staging_comments_list)
