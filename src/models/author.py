564f11e495bdc44a23d72674015c90549bd427dd
Franco Stazzone <fralstazzone@gmail.com>
2025-04-22 21:46:09 -0300

    Adds __eq__ and __hash__ methods to flair and author

--- src/models/author.py
+++ src/models/author.py
@@ -11,3 +11,11 @@ class Author:
         self.author_id = author_id
         self.username = username
         self.flair = flair
+
+    def __eq__(self, other):
+        if isinstance(other, Author):
+            return self.author_id == other.author_id
+        return False
+
+    def __hash__(self):
+        return hash(self.author_id)
--- src/models/flair.py
+++ src/models/flair.py
@@ -5,3 +5,11 @@ class Flair():
                  text: str
                  ):
         self.text = text
+
+    def __eq__(self, other):
+        if isinstance(other, Flair):
+            return self.text == other.text
+        return False
+
+    def __hash__(self):
+        return hash(self.text)
